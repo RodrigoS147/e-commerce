96087f8d3f7f040ea240a3d195d0f99cbf195b25
RodrigoS147 <rodrisantos147@gmail.com>
2023-12-18 00:25:39 -0300

    Add carrito features

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,23 @@ document.getElementById("return2MainPage").addEventListener("click", function() 
   // Redirigir a index.html
   window.location.href = "index.html";
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carrito = document.getElementById("carrito");
+  const toggleCarritoButton = document.getElementById("toggleCarrito");
+
+  // Oculta el carrito al inicio
+  carrito.classList.add("oculto");
+
+  // Muestra u oculta el carrito al hacer clic en el botón de carrito
+  toggleCarritoButton.addEventListener("click", function() {
+    carrito.classList.toggle("visible");
+  });
+
+  // Oculta el carrito al hacer clic en la imagen del carrito dentro del carrito
+  document.querySelector("#carrito img.icono-carrito").addEventListener("click", function() {
+    carrito.classList.remove("visible");
+  });
+});
